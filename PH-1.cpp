9f7a084abb902ddcfc5b82999eadc608c863e44d
probir ghosh <probirghosh@outlook.com>
2022-07-13 19:51:47 +0600

    initial commit

---
+++ PH-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main ()
+{
+    //printf("Hello World");
+    //std :: cout<<"Hello Phitron";
+    //std :: cout << "Hello PK " << "Welcome to C++";
+   // std :: cout << "Hello PK\n" << "Welcome to C++";
+    std :: cout << "Hello PK"<< std ::endl << "Welcome to C++";
+    return 0;
+}
---
+++ PH-2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a = 45;
+ //std :: cout << "The Value " << a <<" is my favourite number" << std :: endl;
+ cout << "This Value " << a <<" is my favourite number" << endl;
+ return 0;
+
+}
